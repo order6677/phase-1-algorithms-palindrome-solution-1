b41fd33570c203766903bdab7f8fb41b10a68b36
awadh <awadh.hassan@student.moringaschool.com>
2022-08-28 11:20:23 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-function isPalindrome(word) {
+function isPalindrome(word) 
   // Write your algorithm here
-}
+//   make a functio  that returns true if a waord is a palindrome and false if not 
+//   that seems if the word is the sam as the word in reverse i should return true
+// }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+//   Add your pseudocode here
+// if the word is the same as the word in reverse i should return  true 
+// reverse the input string 
+
+// if the input is the same as the revresed input 
+// return true
+// else 
+// return false
+
+
+  // Add written explanation of your solution here
+  function reverse(word){
+    const wordArray =word.split("")
+    const reversedWordArray = wordArray.reverse()
+    const reversedWord = reversedWordArray.join("");
+    return reversedWord;
+  }
+  
+  function isPalindrome (word){
+    
+    const reversedWord =reverse(word);
+  
+    if (word === reversedWord){
+      return true;
+    }else {
+      return false;
+    }
+  }
+  
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
